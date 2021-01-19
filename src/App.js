c1adc08d3ea055573dd567ac1a73acf65ecb1e32
CLopez1947 <67842716+CLopez1947@users.noreply.github.com>
2021-01-19 11:03:20 -0800

    Navi changes, need to test on a different computer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import './App.css';
-import Signup from './Components/Signup.js';
-
-
-
+import "./App.css";
+import React from "react";
+import Nav from "./Components/Nav";
+import Home from "./Components/home.js";
+import Signup from "./Components/Signup.js";
+import Login from "./Components/login.js";
+import Post from "./Components/post.js";
+import postFeed from "./Components/postFeed.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 function App() {
-
-
   return (
-    <div className="App">
-      <Signup />
-   
+    <div>
+      <Nav />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/form" component={Signup} />
+          <Route path="/post" component={Post} />
+          <Route path="/postFeed" component={postFeed} />
+        </Switch>
+      </Router>
     </div>
   );
 }
